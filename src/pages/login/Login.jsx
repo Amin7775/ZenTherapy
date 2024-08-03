6d7e1b8899c098be91bfc99f151bb678e48f9826
Amin7775 <amin.dpi24@gmail.com>
2024-08-03 16:30:24 +0600

    Added: sign in image section

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,32 @@
+// import ImageSection from "../../components/loginAndRegisterComponents/Large/ImageSection";
+
+import SignInImageSection from "../../components/loginAndRegisterComponents/Large/SignInImageSection";
+import SignUpForm from "../../components/loginAndRegisterComponents/Shared/SignUpForm";
 
 const Login = () => {
-    return (
-        <div>
-            This is login
+  return (
+    <div>
+      {/* Layout for large device */}
+      <section className="hidden lg:block">
+        {/* container */}
+        <div className="min-h-screen flex items-center justify-center py-20">
+          <section className="flex justify-center items-center gap-32 mx-auto  w-[83%] ">
+            {/* form section */}
+            <div className="flex-1">
+              <SignUpForm></SignUpForm>
+            </div>
+            {/* image section */}
+            <div className="w-[55%]">
+              <SignInImageSection
+                text1={"Sign in"}
+                text2={"to view all the massage therapists"}
+              ></SignInImageSection>
+            </div>
+          </section>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default Login;
