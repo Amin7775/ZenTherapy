3685a7a955ec6e8bb2581acd5e9446de7dad191f
Amin7775 <amin.dpi24@gmail.com>
2024-08-03 16:16:52 +0600

    Updated: changed text alingment in image section component of register/login page

--- src/components/loginAndRegisterComponents/Large/ImageSection.jsx
+++ src/components/loginAndRegisterComponents/Large/ImageSection.jsx
@@ -11,10 +11,10 @@ const ImageSection = ({ text1, text2 }) => {
       />
       {/* text */}
       <div className="absolute flex flex-col items-center justify-center bg-neutral-800 opacity-80  w-[35%] top-[44%] left-[34%] px-8 py-6 rounded-lg hover:opacity-85 border-2 border-transparent hover:border-primary-Color-blue-1 transition-all duration-300 ease-in-out transform hover:scale-105 cursor-pointer">
-        <h3 className="text-primary-Color-blue-2 text-xl font-semibold">
+        <h3 className="text-primary-Color-blue-2 text-xl font-semibold text-center">
           {text1}
         </h3>
-        <h3 className="text-white text-xl mt-1">{text2}</h3>
+        <h3 className="text-white text-xl mt-1 text-center">{text2}</h3>
       </div>
     </div>
   );
